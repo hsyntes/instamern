c546dcadb5622ca5733d8ab38e117cb35052cf17
hsyntes <se.hsyntes@gmail.com>
2023-08-01 19:10:32 +0300

    setting splash screen

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -6,7 +6,7 @@ const Splash = () =>
   createPortal(
     <div id="splash-backdrop" className="bg-white dark:bg-dark">
       <img src={logo} alt="Logo" className="relative" />
-      <Brand className="text-4xl lg:text-5xl absolute bottom-14" />
+      <Brand className="text-4xl lg:text-5xl absolute bottom-24 lg:bottom-14" />
     </div>,
     document.getElementById("modal-root")
   );
